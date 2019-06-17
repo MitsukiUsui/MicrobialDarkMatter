d0effe0ebef8c1f2bff71bbeb928103cf78da126
MitsukiUsui <mitsuo.com.mu@gmail.com>
2019-06-17 23:12:56 +0900

    dev: updated read_mmseqs

--- mylib/df.py
+++ mylib/df.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
-def read_mmseqs(fp):
-    df = pd.read_csv(fp, header=None, sep='\t',
-                     names=["qname", "sname", "identity", "length", "mismatch", "gap",
-                            "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qlength", "slength"])
+MMSEQS_SCHEMA = [("qname", object), ("sname", object),
+                 ("identity", float), ("length", int), ("mismatch", int), ("gap", int),
+                 ("qstart", int), ("qend", int), ("sstart", int), ("send", int),
+                 ("evalue", float), ("bitscore", int), ("qlength", int), ("slength", int)]
+
+def read_mmseqs(fp, **kwargs):
+    # trim schema based on the first line
+    with open(fp, 'r') as f:
+        line = f.readline().strip()
+    schema = MMSEQS_SCHEMA[:len(line.split())]
+    params = {
+        "header": None,
+        "sep": '\t',
+        "names": list(map(lambda x:x[0], schema)),
+        "dtype": dict(schema)
+    }
+    # overwrite params
+    for key, val in kwargs.items():
+        params[key] = val
+    df = pd.read_csv(fp, **params)
     return df
